f3d590bcd3a48dfd98c9884a9134098075176cc4
Tim Wheeler <wheelert@stanford.edu>
2017-12-01 08:51:10 -0800

    ExprRules now a package

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,5 @@
 try Pkg.installed("Vec"); catch
     Pkg.clone("https://github.com/tawheeler/Vec.jl.git")
 end
-try Pkg.installed("ExprRules"); catch
-    Pkg.clone("https://github.com/sisl/ExprRules.jl.git")
-end
 
 Pkg.checkout("Weave")
