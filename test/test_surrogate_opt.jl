879f65648d0bb610b1a6d561e717a4f5a2a7d032
Tim Wheeler <wheelert@stanford.edu>
2017-12-02 12:53:14 -0800

    more tests

--- test/test_surrogate_opt.jl
+++ test/test_surrogate_opt.jl
@@ -53,3 +53,52 @@ let
     @test isnan(u_best)
     @test i_best == 0
 end
+
+let
+    function flower(x; a=1, b=1, c=4)::Float64
+        if isapprox(norm(x), 0.0)
+            return 0.0
+        end
+        return a*norm(x) + b*sin(c*atan2(x[2], x[1]))
+    end
+    f_(x::Vector{Float64}) = flower(x)
+    f_(x1::Float64, x2::Float64) = f_([x1,x2])
+
+    xdomain = ( -3, 3)
+    ydomain = ( -3, 3)
+    y_max = 2.0
+
+    GP = GaussianProcess(x->y_max + 0.5, (x,x′)->exp(-norm(x-x′)), Vector{Float64}[], Float64[], 0.01)
+    β = 3.0
+    n = 51
+    X = Array{Vector{Float64}}(n*n)
+    i = 0
+    for x1 in linspace(xdomain..., n)
+        for x2 in linspace(ydomain..., n)
+            X[i+=1] = [x1,x2]
+        end
+    end
+    i = indmin(norm([-2,1]-x,2) for x in X)
+
+    m = length(X)
+    u, ℓ = fill(Inf, m), fill(-Inf, m)
+    S, M, E = falses(m), falses(m), falses(m)
+
+    srand(0)
+    push!(GP, X[i], f_(X[i]) + randn()*GP.ν)
+    update_confidence_intervals!(GP, X, u, ℓ, β)
+    S[:] = u .≤ y_max
+    best_val, i_best = findmin(u[S])
+    i_best = findfirst(cumsum(S), i_best)
+
+    for k in 2 : prod((4,4))
+        compute_sets!(S, M, E, X, u, ℓ, y_max)
+        i = get_new_query_point(M, E, u, ℓ)
+
+        push!(GP, X[i], f_(X[i]))
+        update_confidence_intervals!(GP, X, u, ℓ, β);
+        S[:] = u .≤ y_max
+        best_val, i_best = findmin(u[S])
+        i_best = findfirst(cumsum(S), i_best)
+    end
+end
--- test/test_uncertaintyprop.jl
+++ test/test_uncertaintyprop.jl
@@ -11,4 +11,41 @@ let
     ret = taylor_approx(f, [0.0,0.0], [1.0,0.5])
     @test isapprox(ret[1], 0.0292, atol=1e-4)
     @test isapprox(ret[2], 0.4991, atol=1e-4)
+
+    @test legendre(1) == Poly([1])
+    @test legendre(2) == Poly([0,1])
+    @test legendre(3) == Poly([-1/2,0,3/2])
+    @test legendre(4) == Poly([0,-3/2,0,5/2])
+
+    @test laguerre(1) == Poly([1])
+    @test laguerre(2) == Poly([1,-1])
+    @test laguerre(3) == Poly([1,-2,1/2])
+    @test laguerre(4) == Poly([1,-3,3/2,-1/6])
+
+    @test hermite(1) == Poly([1])
+    @test hermite(2) == Poly([0,1])
+    @test hermite(3) == Poly([-1,0,1])
+    @test hermite(4) == Poly([0,-3,0,1])
+
+    # for makepoly in [legendre, laguerre, hermite]
+    #     for i in 1 : 3
+    #         bs = [makepoly(j) for j in 1 : i]
+    #         @test orthogonal_recurrence(bs, x->pdf(Uniform(-1,1),x), (-1,1), 1e-16) ≈ makepoly(i+1).a
+    #     end
+    # end
+
+
+    quadrule  = quadrule_legendre(3)
+    i = findfirst(quadrule.xs, 0.0)
+    @test isapprox(quadrule.ws[i], 8/9, atol=1e-8)
+    i = findfirst(x->isapprox(x,  sqrt(3/5), atol=1e-6), quadrule.xs)
+    @test isapprox(quadrule.ws[i], 5/9, atol=1e-8)
+    i = findfirst(x->isapprox(x, -sqrt(3/5), atol=1e-6), quadrule.xs)
+    @test isapprox(quadrule.ws[i], 5/9, atol=1e-8)
+
+    f = x -> x^5- 2x^4 + 3x^3 + 5x^2 -x + 4
+    @test isapprox(quadint(f, quadrule_legendre(3)), 10.533, atol=1e-3)
+    @test isapprox(quadint(f, quadrule_legendre(3), -3, 5), 1820.8, atol=1e-3)
+
+
 end
