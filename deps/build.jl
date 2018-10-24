5d52a184359a6a6bbe493a711f310a8ad5f2d6b7
Tim Wheeler <wheelert@stanford.edu>
2018-10-23 19:39:54 -0700

    install exprrules. Clean out base packages

--- deps/build.jl
+++ deps/build.jl
@@ -2,4 +2,6 @@ try Pkg.installed("Vec"); catch
     Pkg.clone("https://github.com/tawheeler/Vec.jl.git")
 end
 
-Pkg.checkout("Weave")
+try Pkg.installed("ExprRules"); catch
+    Pkg.clone("https://github.com/sisl/ExprRules.jl.git")
+end
